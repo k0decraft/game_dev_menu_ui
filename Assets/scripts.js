3c03178003528ef716787c519095329bae82bbef
Taure Anthony <codekraft1982@gmail.com>
2015-08-25 14:49:30 -0400

    game menu/ui
    game menu and ui

---
+++ Assets/scripts.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+// START SCREEN ANIMATION & UI MENU 
+
+
+function Start () {
+
+	// Use this for initialization
+	void Start () {
+    Debug.Log("I am alive!");
+}
+
+
+}
+
+function Update () {
+
+}
